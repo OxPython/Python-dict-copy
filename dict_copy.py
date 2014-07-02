0bed43cbae7bc3cfe61a19aa8fa93d58fd3168d2
Emerson <viejoemer@gmail.com>
2014-07-02 14:00:00 -0500

    How to copy or clone a dict in Python?

---
+++ dict_copy.py
@@ -0,0 +1,27 @@
+'''
+Created on Jul 2, 2014
+
+@author: viejoemer
+
+How to copy or clone a dict in Python?
+
+¿Cómo copiar o clonar un diccionario en Python?
+
+The method copy() returns a shallow copy of the dictionary.
+'''
+
+#dict with three values and keys
+d = {"one":1, "two":2, "three":3}
+print(d)
+
+#copy a dict into another variable
+d2 = d.copy()
+print(d2)
+
+#Change dict d, insert a new value
+d["Four"] = 4
+print(d)
+
+#Change dict d2, insert a new value
+d2["Five"] = 5
+print(d2)
